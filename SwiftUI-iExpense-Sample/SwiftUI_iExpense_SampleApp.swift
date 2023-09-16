11ed333e62852129b5b2a8629f8a530fef72d71f
ekayaint <ekayaint@gmail.com>
2023-09-16 14:52:46 +0300

    Initial Commit

---
+++ SwiftUI-iExpense-Sample/SwiftUI_iExpense_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iExpense_SampleApp.swift
+//  SwiftUI-iExpense-Sample
+//
+//  Created by ekayaint on 16.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iExpense_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
